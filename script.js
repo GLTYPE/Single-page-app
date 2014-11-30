ca83d50d6aa6a561c48d1e30517836bb43863070
Gilles Tual <gilles.tual@epitech.eu>
2014-11-30 21:10:06 +0800

    structure of single page app

---
+++ script.js
@@ -0,0 +1,41 @@
+﻿var gltypeApp = angular.module('gltypeApp', ['ngRoute']);
+
+gltypeApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+    });
+
+gltypeApp.controller('mainController', function($scope) {
+		$scope.webmaster = "Gilles TUAL";
+	});
+
+gltypeApp.controller('aboutController', function($scope) {
+		$scope.webmaster = "Gilles TUAL";
+		$scope.members = [{firstname:'Gilles', lastname:'TUAL', img: './img/tual_g.jpg', role: 'Project Leader / Designer', age:'22'},
+        {firstname: '一同', lastname:'王', img: './img/empty.jpg', role: 'Android Developer', age:'24'},
+        {firstname: 'Thomas', lastname:'LACROIX', img: './img/empty.jpg', role: 'Web Developer', age:'21'},
+        {firstname: 'Morgan', lastname:'', img: './img/empty.jpg', role: 'Android Developer', age:'?'},
+        {firstname: 'Pierre', lastname:'MEDARD', img: './img/empty.jpg', role: 'API Developer', age:'21'},
+        {firstname: 'Yan', lastname:'', img: './img/empty.jpg', role: 'Android Developer', age:'?'}];
+    });
+
+   	gltypeApp.controller('contactController', function($scope) {
+   		$scope.webmaster = "Gilles TUAL";
+    });
